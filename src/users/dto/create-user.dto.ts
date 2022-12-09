741cbe9797ef8aac8938559683e9e346147b1906
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-12-09 12:27:07 -0300

    feat: implemented dto and entity

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,45 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({
+    description: 'Nome do usuário',
+    example: 'Mateus Jacinto Santiago',
+  })
+  name: string;
+
+  @ApiProperty({
+    description: 'CPF',
+    example: '00000000000',
+  })
+  cpf: string;
+
+  @ApiProperty({
+    description: 'Email',
+    example: 'usuario@usuario.com.br',
+  })
+  email: string;
+
+  @ApiProperty({
+    description: 'Idade',
+    example: 32,
+  })
+  idade: string;
+
+  @ApiProperty({
+    description: '',
+    example: '',
+  })
+  role: string;
+
+  @ApiProperty({
+    description: 'Senha do usuário',
+    example: 'secret',
+  })
+  password: string;
+
+  @ApiProperty({
+    description: 'confirmação da senha',
+    example: 'secret',
+  })
+  confirmPassword: string;
+}
---
+++ src/users/entities/user-entity.ts
@@ -0,0 +1,9 @@
+export class UserEntity {
+    id:string;
+    name: string;
+    cpf: string;
+    email: string;
+    idade: string;
+    role: string;
+    password: string;
+}
