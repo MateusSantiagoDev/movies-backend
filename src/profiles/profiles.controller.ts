a000fa6d11fcf2b800d0d6c570548ed47ee9f44b
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-12-11 17:57:18 -0300

    feat: update profile method

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,8 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ProfilesService } from './profiles.service';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { exceptionhandling } from 'src/utils/exceptions/exceptionhandling';
 
 @ApiTags('profiles')
 @Controller('profiles')
@@ -10,42 +19,62 @@ export class ProfilesController {
   constructor(private readonly service: ProfilesService) {}
 
   @ApiOperation({
-    summary: 'Criar um perfil'
+    summary: 'Criar um perfil',
   })
   @Post()
   create(@Body() dto: CreateProfileDto) {
-    return this.service.create(dto);
+    try {
+      return this.service.create(dto);
+    } catch (err) {
+      exceptionhandling(err);
+    }
   }
 
   @ApiOperation({
-    summary: 'Visualizar todos os perfils'
+    summary: 'Visualizar todos os perfils',
   })
   @Get()
   findAll() {
-    return this.service.findAll();
+    try {
+      return this.service.findAll();
+    } catch (err) {
+      exceptionhandling(err);
+    }
   }
 
   @ApiOperation({
-    summary: 'Buscar um perfil pelo ID'
+    summary: 'Buscar um perfil pelo ID',
   })
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.service.findOne(id);
+    try {
+      return this.service.findOne(id);
+    } catch (err) {
+      exceptionhandling(err);
+    }
   }
 
   @ApiOperation({
-    summary: 'Editar um perfil pelo ID'
+    summary: 'Editar um perfil pelo ID',
   })
   @Patch(':id')
   update(@Param('id') id: string, @Body() dto: UpdateProfileDto) {
-    return this.service.update(id, dto);
+    try {
+      return this.service.update(id, dto);
+    } catch (err) {
+      exceptionhandling(err);
+    }
   }
 
   @ApiOperation({
-    summary: 'Deletar um perfil pelo ID'
+    summary: 'Deletar um perfil pelo ID',
   })
   @Delete(':id')
   delete(@Param('id') id: string) {
-    return this.service.delete(id);
+    try {
+      return this.service.delete(id);
+    } catch (err) {
+      exceptionhandling(err);
+    }
   }
 }
--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { randomUUID } from 'crypto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Exceptions } from 'src/utils/exceptions/exception.class';
+import { Exception } from 'src/utils/exceptions/exceptions';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
 
@@ -9,114 +11,152 @@ import { UpdateProfileDto } from './dto/update-profile.dto';
 export class ProfilesService {
   constructor(private readonly prisma: PrismaService) {}
   create(dto: CreateProfileDto) {
-    const data: Prisma.ProfilesCreateInput = {
-      id: randomUUID(),
-      user: {
-        connect: {
-          id: dto.userId,
-        },
-      },
-      movie: {
-        connect: dto.movie.map((el) => ({
-          id: el,
-        })),
-      },
-      serie: {
-        connect: dto.serie.map((el) => ({
-          id: el,
-        })),
-      },
-      anime: {
-        connect: dto.anime.map((el) => ({
-          id: el,
-        })),
-      },
-    };
-
-    return this.prisma.profiles.create({
-      data,
-      select: {
-        id: true,
+    try {
+      const data: Prisma.ProfilesCreateInput = {
+        id: randomUUID(),
         user: {
-          select: {
-            name: true,
+          connect: {
+            id: dto.userId,
           },
         },
         movie: {
-          select: {
-            title: true,
-          },
+          connect: dto.movie.map((el) => ({
+            id: el,
+          })),
         },
         serie: {
-          select: {
-            title: true,
-          },
+          connect: dto.serie.map((el) => ({
+            id: el,
+          })),
         },
         anime: {
-          select: {
-            title: true,
+          connect: dto.anime.map((el) => ({
+            id: el,
+          })),
+        },
+      };
+
+      return this.prisma.profiles.create({
+        data,
+        select: {
+          id: true,
+          user: {
+            select: {
+              name: true,
+            },
+          },
+          movie: {
+            select: {
+              title: true,
+            },
+          },
+          serie: {
+            select: {
+              title: true,
+            },
+          },
+          anime: {
+            select: {
+              title: true,
+            },
           },
         },
-      },
-    });
+      });
+    } catch (err) {
+      throw new Exceptions(Exception.DatabaseException);
+    }
   }
 
   findAll() {
-    return this.prisma.profiles.findMany({
-      select: {
-        id: true,
-        user: {
-          select: {
-            name: true,
-            email: true,
-            idade: true,
+    try {
+      return this.prisma.profiles.findMany({
+        select: {
+          id: true,
+          user: {
+            select: {
+              name: true,
+              email: true,
+              idade: true,
+            },
+          },
+          movie: {
+            select: {
+              title: true,
+              description: true,
+              avaliation: true,
+              image: true,
+            },
+          },
+          serie: {
+            select: {
+              title: true,
+              description: true,
+              avaliation: true,
+              image: true,
+            },
+          },
+          anime: {
+            select: {
+              title: true,
+              description: true,
+              avaliation: true,
+              image: true,
+            },
           },
         },
-        movie: {
-          select: {
-            title: true,
-            description: true,
-            avaliation: true,
-            image: true,
-          },
-        },
-        serie: {
-          select: {
-            title: true,
-            description: true,
-            avaliation: true,
-            image: true,
-          },
-        },
-        anime: {
-          select: {
-            title: true,
-            description: true,
-            avaliation: true,
-            image: true,
-          },
-        },
-      },
-    });
+      });
+    } catch (err) {
+      throw new Exceptions(Exception.DatabaseException);
+    }
   }
 
   findOne(id: string) {
-    return this.prisma.profiles.findUnique({
-      where: { id },
-      include: {
-        user: true,
-        movie: true,
-        serie: true,
-        anime: true,
-      },
-    });
+    try {
+      return this.prisma.profiles.findUnique({
+        where: { id },
+        include: {
+          user: true,
+          movie: true,
+          serie: true,
+          anime: true,
+        },
+      });
+    } catch (err) {
+      throw new Exceptions(Exception.DatabaseException);
+    }
   }
 
   update(id: string, dto: UpdateProfileDto) {
-    return `This action updates a #${id} profile`;
+    try {
+      return this.prisma.profiles.update({
+        where: { id },
+        data: {
+          movie: {
+            connect: dto.movie.map((el) => {
+              return { id: el };
+            }),
+          },
+          serie: {
+            connect: dto.serie.map((el) => {
+              return { id: el };
+            }),
+          },
+          anime: {
+            connect: dto.anime.map((el) => {
+              return { id: el };
+            }),
+          },
+        },
+      });
+    } catch (err) {
+      throw new Exceptions(Exception.DatabaseException);
+    }
   }
-
   delete(id: string) {
-    return `This action removes a #${id} profile`;
+    try {
+      return this.prisma.profiles.delete({ where: { id } });
+    } catch (err) {
+      throw new Exceptions(Exception.DatabaseException);
+    }
   }
 }
